5cdc82fd61fb960fe0a45f1534c4cbea9a9da6fe
Rasmus Holm <rasho381@student.liu.se>
2017-11-08 18:30:31 +0100

    fix indenting

--- new-lab6/filter.c
+++ new-lab6/filter.c
@@ -46,7 +46,7 @@ int init_OpenCL()
   // Get the GPU device
   ciErrNum = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
   printCLError(ciErrNum,1);
-  
+
   // create the OpenCL context on the device
   cxGPUContext = clCreateContext(0, 1, &device, NULL, NULL, &ciErrNum);
   printCLError(ciErrNum,2);
@@ -54,15 +54,15 @@ int init_OpenCL()
   ciErrNum = clGetDeviceInfo(device,CL_DEVICE_MAX_WORK_GROUP_SIZE,sizeof(size_t),&noWG,NULL);
   printCLError(ciErrNum,3);
   printf("maximum number of workgroups: %d\n", (int)noWG);
-  
+
   // create command queue
   commandQueue = clCreateCommandQueue(cxGPUContext, device, 0, &ciErrNum);
   printCLError(ciErrNum,4);
-  
+
   // 0 means no error
   if (ciErrNum == CL_SUCCESS) return 0;
   else
-  	return -1; // error
+    return -1; // error
 }
 
 int readAndBuildKernel(char *filename)
@@ -70,15 +70,15 @@ int readAndBuildKernel(char *filename)
   cl_int ciErrNum = CL_SUCCESS;
   size_t kernelLength;
   char *source;
-  
+
   source = readFile(filename);
   kernelLength = strlen(source);
-  
+
   // create the program
   theProgram = clCreateProgramWithSource(cxGPUContext, 1, (const char **)&source, 
                                                     &kernelLength, &ciErrNum);
   printCLError(ciErrNum,5);
-    
+
   // build the program
   ciErrNum = clBuildProgram(theProgram, 0, NULL, NULL, NULL, NULL);
   if (ciErrNum != CL_SUCCESS)
@@ -90,22 +90,22 @@ int readAndBuildKernel(char *filename)
     printf("\nBuild Log:\n%s\n\n", (char *)&cBuildLog);
     return -1;
   }
-  
+
   theKernel = clCreateKernel(theProgram, "filter", &ciErrNum);
   printCLError(ciErrNum,6);
-  
+
   //Discard temp storage
   free(source);
-  
+
   return 0;
 }
 
 void close_OpenCL()
 {
-	if (theKernel) clReleaseKernel(theKernel);
-	if (theProgram) clReleaseProgram(theProgram);
-	if (commandQueue) clReleaseCommandQueue(commandQueue);
-	if (cxGPUContext) clReleaseContext(cxGPUContext);
+  if (theKernel) clReleaseKernel(theKernel);
+  if (theProgram) clReleaseProgram(theProgram);
+  if (commandQueue) clReleaseCommandQueue(commandQueue);
+  if (cxGPUContext) clReleaseContext(cxGPUContext);
 }
 
 // Global variables for image data
@@ -118,52 +118,52 @@ cl_uint n, m; // Image size
 ////////////////////////////////////////////////////////////////////////////////
 void computeImages()
 {
-	image = readppm("maskros512.ppm", (int *)&n, (int *)&m);
-	out = (unsigned char*) malloc(n*m*3);
-	cl_mem in_data, out_data;
-	cl_int ciErrNum = CL_SUCCESS;
-	
-	// Create space for data and copy image to device (note that we could also use clEnqueueWriteBuffer to upload)
-	in_data = clCreateBuffer(cxGPUContext, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR,
-		3*n*m * sizeof(unsigned char), image, &ciErrNum);
-	printCLError(ciErrNum,6);
-	out_data = clCreateBuffer(cxGPUContext, CL_MEM_WRITE_ONLY,
-		3*n*m * sizeof(unsigned char), NULL, &ciErrNum);
-	printCLError(ciErrNum,7);
+  image = readppm("maskros512.ppm", (int *)&n, (int *)&m);
+  out = (unsigned char*) malloc(n*m*3);
+  cl_mem in_data, out_data;
+  cl_int ciErrNum = CL_SUCCESS;
 
-	// set the args values
-	ciErrNum  = clSetKernelArg(theKernel, 0, sizeof(cl_mem),  (void *) &in_data);
-	ciErrNum |= clSetKernelArg(theKernel, 1, sizeof(cl_mem),  (void *) &out_data);
-	ciErrNum |= clSetKernelArg(theKernel, 2, sizeof(cl_uint), (void *) &n);
-	ciErrNum |= clSetKernelArg(theKernel, 3, sizeof(cl_uint), (void *) &m);
-	printCLError(ciErrNum,8);
+  // Create space for data and copy image to device (note that we could also use clEnqueueWriteBuffer to upload)
+  in_data = clCreateBuffer(cxGPUContext, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR,
+    3*n*m * sizeof(unsigned char), image, &ciErrNum);
+  printCLError(ciErrNum,6);
+  out_data = clCreateBuffer(cxGPUContext, CL_MEM_WRITE_ONLY,
+    3*n*m * sizeof(unsigned char), NULL, &ciErrNum);
+  printCLError(ciErrNum,7);
 
-	// Computing arrangement
-	//size_t localWorkSize, globalWorkSize;
-	const size_t globalWorkSize[3] = {512, 512, 1};
-	const size_t localWorkSize[3] = {16, 16, 1};
+  // set the args values
+  ciErrNum  = clSetKernelArg(theKernel, 0, sizeof(cl_mem),  (void *) &in_data);
+  ciErrNum |= clSetKernelArg(theKernel, 1, sizeof(cl_mem),  (void *) &out_data);
+  ciErrNum |= clSetKernelArg(theKernel, 2, sizeof(cl_uint), (void *) &n);
+  ciErrNum |= clSetKernelArg(theKernel, 3, sizeof(cl_uint), (void *) &m);
+  printCLError(ciErrNum,8);
 
-	printf("Startup time %lf\n", GetSeconds());
+  // Computing arrangement
+  //size_t localWorkSize, globalWorkSize;
+  const size_t globalWorkSize[3] = {512, 512, 1};
+  const size_t localWorkSize[3] = {16, 16, 1};
 
-	// Compute!
-	cl_event event;
-	ResetMilli();
-	ciErrNum = clEnqueueNDRangeKernel(commandQueue, theKernel, 2, NULL, &globalWorkSize[0], &localWorkSize[0], 0, NULL, &event);
-	printCLError(ciErrNum,9);
+  printf("Startup time %lf\n", GetSeconds());
 
- 	ciErrNum = clWaitForEvents(1, &event); // Synch
-	printCLError(ciErrNum,10);
-	printf("time %lf\n", GetSeconds());
+  // Compute!
+  cl_event event;
+  ResetMilli();
+  ciErrNum = clEnqueueNDRangeKernel(commandQueue, theKernel, 2, NULL, &globalWorkSize[0], &localWorkSize[0], 0, NULL, &event);
+  printCLError(ciErrNum,9);
 
-	ciErrNum = clEnqueueReadBuffer(commandQueue, out_data, CL_TRUE, 0, 3*n*m * sizeof(unsigned char), out, 0, NULL, &event);
-	printCLError(ciErrNum,11);
-	clWaitForEvents(1, &event); // Synch
-	printCLError(ciErrNum,10);
-    
-	clReleaseMemObject(in_data);
-	clReleaseMemObject(out_data);
-	
-	return;
+   ciErrNum = clWaitForEvents(1, &event); // Synch
+  printCLError(ciErrNum,10);
+  printf("time %lf\n", GetSeconds());
+
+  ciErrNum = clEnqueueReadBuffer(commandQueue, out_data, CL_TRUE, 0, 3*n*m * sizeof(unsigned char), out, 0, NULL, &event);
+  printCLError(ciErrNum,11);
+  clWaitForEvents(1, &event); // Synch
+  printCLError(ciErrNum,10);
+
+  clReleaseMemObject(in_data);
+  clReleaseMemObject(out_data);
+
+  return;
 }
 
 
@@ -171,36 +171,36 @@ void computeImages()
 // Display images
 void Draw()
 {
-// Dump the whole picture onto the screen.	
-	glClearColor( 0.0, 0.0, 0.0, 1.0 );
-	glClear( GL_COLOR_BUFFER_BIT );
-	glRasterPos2f(-1, -1);
-	glDrawPixels( n, m, GL_RGB, GL_UNSIGNED_BYTE, image );
-	glRasterPos2i(0, -1);
-	glDrawPixels( n, m, GL_RGB, GL_UNSIGNED_BYTE, out );
-	glFlush();
+// Dump the whole picture onto the screen.
+  glClearColor( 0.0, 0.0, 0.0, 1.0 );
+  glClear( GL_COLOR_BUFFER_BIT );
+  glRasterPos2f(-1, -1);
+  glDrawPixels( n, m, GL_RGB, GL_UNSIGNED_BYTE, image );
+  glRasterPos2i(0, -1);
+  glDrawPixels( n, m, GL_RGB, GL_UNSIGNED_BYTE, out );
+  glFlush();
 }
 
 // Main program, inits
-int main( int argc, char** argv) 
+int main( int argc, char** argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode( GLUT_SINGLE | GLUT_RGBA );
-	glutInitWindowSize( 1024, 512 );
-	glutCreateWindow("CUDA on live GL");
-	glutDisplayFunc(Draw);
+  glutInit(&argc, argv);
+  glutInitDisplayMode( GLUT_SINGLE | GLUT_RGBA );
+  glutInitWindowSize( 1024, 512 );
+  glutCreateWindow("CUDA on live GL");
+  glutDisplayFunc(Draw);
 
-	ResetMilli();
-	if (init_OpenCL() != 0)
-	{
-		printf("OpenCL could not be initialized!\n");
-		close_OpenCL();
-		return 0;
-	}
-	readAndBuildKernel("filter.cl");
-	computeImages();
-	close_OpenCL();
+  ResetMilli();
+  if (init_OpenCL() != 0)
+  {
+    printf("OpenCL could not be initialized!\n");
+    close_OpenCL();
+    return 0;
+  }
+  readAndBuildKernel("filter.cl");
+  computeImages();
+  close_OpenCL();
 
-	glutMainLoop();
-	return 0;
+  glutMainLoop();
+  return 0;
 }
